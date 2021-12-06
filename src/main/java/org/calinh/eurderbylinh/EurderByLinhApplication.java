bd5b00bde2ce8cfb65e56cdbdc28ca0cf37c2701
Linh P. Nguyen <nguyenlp.info@gmail.com>
2021-12-06 10:21:17 +0100

    build: ADD repo, jenkins, readme and gitignore

---
+++ src/main/java/org/calinh/eurderbylinh/EurderByLinhApplication.java
@@ -0,0 +1,13 @@
+package org.calinh.eurderbylinh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurderByLinhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurderByLinhApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/calinh/eurderbylinh/EurderByLinhApplicationTests.java
@@ -0,0 +1,13 @@
+package org.calinh.eurderbylinh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurderByLinhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
